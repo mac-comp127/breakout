340066fbf4e9f9e695c77d7f6d48613311b308f2
Paul Cantrell <cantrell@pobox.com>
2019-10-29 14:49:42 -0500

    Updated starter code for 127

---
+++ src/breakout/BreakoutGame.java
@@ -0,0 +1,26 @@
+package breakout;
+
+
+import comp127graphics.CanvasWindow;
+
+/**
+ * The game of Breakout.
+ *
+ */
+public class BreakoutGame {
+
+    private static final int CANVAS_WIDTH = 600;
+    private static final int CANVAS_HEIGHT = 800;
+
+    private CanvasWindow canvas;
+
+    public BreakoutGame() {
+        canvas = new CanvasWindow("Breakout!", CANVAS_WIDTH, CANVAS_HEIGHT);
+
+    }
+
+    public static void main(String[] args){
+        new BreakoutGame();
+    }
+
+}
