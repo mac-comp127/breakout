6031adbb58bd4c43fc678ee64427313649007626
Paul Cantrell <cantrell@pobox.com>
2020-10-04 21:14:35 -0500

    VS Code + Gradle migration

--- src/breakout/BreakoutGame.java
+++ src/breakout/BreakoutGame.java
@@ -1,14 +1,11 @@
 package breakout;
 
-
-import comp127graphics.CanvasWindow;
+import edu.macalester.graphics.CanvasWindow;
 
 /**
  * The game of Breakout.
- *
  */
 public class BreakoutGame {
-
     private static final int CANVAS_WIDTH = 600;
     private static final int CANVAS_HEIGHT = 800;
 
@@ -22,5 +19,4 @@ public class BreakoutGame {
     public static void main(String[] args){
         new BreakoutGame();
     }
-
 }
